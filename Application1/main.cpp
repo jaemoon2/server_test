e7afa018f34d9431f0d9e6b5572b82a32bd7725b
JaeMoon <jaemoon@hanmail.net>
2016-02-29 19:27:48 +0900

    11

--- Application1/main.cpp
+++ Application1/main.cpp
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
 	printf("hello world\n");
 
-	printf("hello world\n");
+	int i = 1;
+	int *pi = &i;
 
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
+	printf(" %d \n", pi);
+	printf(" %d \n", pi+0);
+	printf(" %d \n", pi+1);
+	printf(" %d \n", pi+2);
+	printf(" %d \n", *(pi+1));
 
-	return 0;
+
+	printf(" %d \n", pi[1]);
+	
+
+	system("pause");
+	//return 0;
 }
